3247ff3a078c82cfc66ad5537699b79592421298
danish khan <danishshoukat1993@gmail.com>
2023-11-11 18:27:54 +0500

    making api for product fs modules (inprogress)

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,21 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = 3000;
+const path = require("path");
+const fs = require("fs");
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+
+const filePath = path.join(process.cwd(), "data", "product.json");
+
+app.get("/", (req, res) => {
+   async fs.readFile(filePath, "utf8" ,(error, data) => {
+      const data =await JSON.parse(data);
+      console.log(data.product,"====>>>>");
+      return data
+  });
+  res.send(data);
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
